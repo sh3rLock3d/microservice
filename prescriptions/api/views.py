aef32c30bce7bf599eafda2d5cdef43e26ee28b7
esmaeil1377 <pahang1378@gmail.com>
2021-12-20 23:53:04 +0330

    add api of prescription microservice

--- prescriptions/api/views.py
+++ prescriptions/api/views.py
@@ -11,3 +11,14 @@ def index(request):
     #return HttpResponse('res':"Hello, world. You're at the polls index.")
 
 
+@csrf_exempt 
+def dr_addPrescription(request):
+    return JsonResponse({'foo':'bar'})
+
+@csrf_exempt 
+def p_listpres(request):
+    return JsonResponse({'foo':'bar'})
+
+@csrf_exempt 
+def admin_getstatistics(request):
+    return JsonResponse({'foo':'bar'})
